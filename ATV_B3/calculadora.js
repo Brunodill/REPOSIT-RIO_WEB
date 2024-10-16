c6ab3295aeb9262dc29a482865ef97896e6cc85c
Bruno Dill <159143461+Brunodill@users.noreply.github.com>
2024-10-16 08:20:16 -0300

    Update calculadora.js

--- ATV_B3/calculadora.js
+++ ATV_B3/calculadora.js
@@ -3,11 +3,13 @@ function limpar() {
 }
 
 function botao(value) { //define a funçao botao com o value como parâmetro
-    document.getElementById("result").value += value; //Adiciona o 'value' passado ao final do conteúdo atual do visor
+    document.getElementById("result").value += value; //adiciona o 'value' passado ao final do conteúdo atual do visor
 }
 
 function calculate() { 
-    const val = document.getElementById("result").value; // Armazena o valor atual do visor na variável 'val'
-    const final = eval(val); //Avalia a expressão armazenada em 'val' e armazena o resultado em 'final'
-    document.getElementById("result").value = final; //Define o valor do visor como o resultado calculado 'final'
+    const val = document.getElementById("result").value; //Armazena o valor atual do visor na variável "val"
+    const resultado = eval(val); //Avalia a expressão armazenada em "val" e armazena o resultado em "resultado"
+    //eval() irá transformar a operação armazenada em "val" em uma expressão matematica, realizando a operação e armazenando o resultado em "resultado"
+    
+    document.getElementById("result").value = final; //Define o valor do visor como o resultado armazenado em "resultado"
 }
